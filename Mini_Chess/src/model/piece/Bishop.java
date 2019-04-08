9f62b923149983235917567c8a3f3b8461f265d6
mateodiaz <mateodiaz@mateos-mbp.modem>
2019-04-08 22:22:39 +1000

    big dog

--- Mini_Chess/src/model/piece/Bishop.java
+++ Mini_Chess/src/model/piece/Bishop.java
@@ -2,4 +2,8 @@ package model.piece;
 
 public class Bishop extends AbstractPiece {
 
+	public Bishop(String color, int posX, int posY) {
+		super(color, posX, posY);
+	}
+
 }
--- Mini_Chess/src/model/piece/Knight.java
+++ Mini_Chess/src/model/piece/Knight.java
@@ -2,4 +2,8 @@ package model.piece;
 
 public class Knight extends AbstractPiece {
 
+	public Knight(String color, int posX, int posY) {
+		super(color, posX, posY);
+	}
+
 }
