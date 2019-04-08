0ca2a8dadee511418c0f4aa9cde3c20302111993
s3707187 <s3707187@student.rmit.edu.au>
2019-04-08 22:27:57 +1000

    GameEngineImpl progress

--- Mini_Chess/src/model/GameEngine.java
+++ Mini_Chess/src/model/GameEngine.java
@@ -6,25 +6,26 @@ import view.UserInterfaceManager;
 
 public interface GameEngine {
 
-	public void addPlayer();
-	
+	public void addPlayer(Player p);
+
 	public Player getPlayer(String id);
-	
+
 	public Map<String, Player> getAllPlayers();
-	
+
 	public boolean removePlayer(Player p);
-	
+
 	public Player getCurrentPlayer();
-	
+
 	public void addUIManager(UserInterfaceManager manager);
-	
+
 	public void removeUIManager(UserInterfaceManager manager);
-	
+
 	public int calculatePlayerPoints();
-	
+
 	public GameBoard getGameBoard();
-	
+
 	public boolean movePiece();
-	
-	
+
+	public void setMaxTurns(int turns);
+
 }
--- Mini_Chess/src/model/GameEngineImpl.java
+++ Mini_Chess/src/model/GameEngineImpl.java
@@ -1,25 +1,31 @@
 package model;
 
-
 import java.util.*;
 import java.io.*;
-
 import view.UserInterfaceManager;
 
 public class GameEngineImpl implements GameEngine {
-
+	
+	private static int MAX_PLAYERS = 2;
 	private Map<String, Player> players;
 	private List<UserInterfaceManager> userInterfaceManagers;
+	private GameBoard mainBoard;
+	int maxTurns;
+	private Player currentPlayer;
+	
+	
 	
 	public GameEngineImpl() {
 		players = new HashMap<String, Player>();
 		userInterfaceManagers = new LinkedList<>();
+		mainBoard = new GameBoardImpl();
 	}
 	
 	@Override
-	public void addPlayer() {
-		// TODO Auto-generated method stub
-		
+	public void addPlayer(Player p) {
+		if (players.size() < MAX_PLAYERS){
+			players.put(p.getID(), p);
+		} 
 	}
 
 	@Override
@@ -38,7 +44,7 @@ public class GameEngineImpl implements GameEngine {
 
 	@Override
 	public boolean removePlayer(Player p) {
-		if(players.remove(p.getName()) == null) {
+		if(players.remove(p.getID()) == null) {
 			return false;
 		}
 		return true;
@@ -60,13 +66,13 @@ public class GameEngineImpl implements GameEngine {
 	@Override
 	public int calculatePlayerPoints() {
 		// TODO Auto-generated method stub
+		// Loop through pieces of opposite player?
 		return 0;
 	}
 
 	@Override
 	public GameBoard getGameBoard() {
-		// TODO Auto-generated method stub
-		return null;
+		return mainBoard;
 	}
 
 	
@@ -82,5 +88,11 @@ public class GameEngineImpl implements GameEngine {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	@Override
+	public void setMaxTurns(int turns) {
+		maxTurns = turns;
+	}
 
+	
 }
