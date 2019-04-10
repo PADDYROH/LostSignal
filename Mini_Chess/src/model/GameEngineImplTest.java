825189dd1040eb5ec2f982c8d7599eab14adb19c
Pat's PC <Pat's PC@DESKTOP-PLGUNPC>
2019-04-10 13:04:31 +1000

    mockup print statement

--- Mini_Chess/src/model/GameEngineImplTest.java
+++ Mini_Chess/src/model/GameEngineImplTest.java
@@ -5,35 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class GameEngineImplTest extends GameEngineImpl {
+public class GameEngineImplTest{
 
-	BasePlayer p;
+	BasePlayer player;
 	GameEngineImpl gameEngine;
 
 	@Before
 	public void setUp() throws Exception {
 
-		p = new BasePlayer("30", "ChessGuy", 100);
+		player = new BasePlayer("100",10, "", 100);
 		gameEngine = new GameEngineImpl();
 
 	}
 
 	@Test
 	public void testGetID() {
-		assertEquals("30", p.getID());
+		assertEquals("30", player.getID());
 	}
 
 	@Test
 	public void testgetAllPlayer() {
-		gameEngine.addPlayer(p);
+		gameEngine.addPlayer(player);
 		assertEquals(1, gameEngine.getAllPlayers().size());
 
 	}
 
 	@Test
+	public void testRemovePlayer() {
+		gameEngine.removePlayer(player);
+		assertEquals(false, gameEngine.removePlayer(player));
+	}
+
+	@Test
 	public void testAddPlayer() {
-		gameEngine.addPlayer(p);
-		assertEquals(true, gameEngine.getAllPlayers().containsKey(p.getID()));
+		gameEngine.addPlayer(player);
+		assertEquals(true, gameEngine.getAllPlayers().containsKey(player.getID()));
 	}
 
 }
--- Mini_Chess/src/view/CLIManager.java
+++ Mini_Chess/src/view/CLIManager.java
@@ -1,5 +1,25 @@
 package view;
 
 public class CLIManager implements UserInterfaceManager {
+	private int boardwidth;
+	private int boardlength;
+	
+	public CLIManager(int boardlength,  int boardwidth) {
 
+	this.boardlength = boardlength;
+	this.boardwidth = boardwidth;
+	
+		
+		String[] chess = new String[boardlength];
+		for (int row = 0; row < boardwidth; row++) {
+			System.out.println("");
+			System.out.println("____________");
+			for (int column = 0; column < boardwidth; column++) {
+				System.out.println("| "+ "CLIManager(piece.getposX, piece.getY)"+ "   ");
+			}
+				System.out.println("|");
+		}
+		System.out.println(" ");
+		System.out.println("____________");
+	}
 }
