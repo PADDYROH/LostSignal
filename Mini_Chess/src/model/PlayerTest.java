c9882e278457000aa607afbc599544e56a111b27
Pat's PC <Pat's PC@DESKTOP-PLGUNPC>
2019-04-10 13:01:14 +1000

    player test

--- Mini_Chess/src/model/PlayerTest.java
+++ Mini_Chess/src/model/PlayerTest.java
@@ -1,78 +1,83 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
-class PlayerTest {
+import org.junit.Test;
+
+
+public class PlayerTest{
+	
 	BasePlayer player;
-	//when
-	player = new BasePlayer("100",10, "", 100);
-	//public BasePlayer(String playerID, int playerPasswordHash, String playerName, int playerPoints) {
-		
-		@Test
-		public void testsetName() {
-			//when
-			player.setName("joe");
-			//then
-			assertEquals("joe", player.getName());
-		}
-		
-		public void testgetName() {
-			//when
-			player.setName("joe");
-			//then
-			assertEquals("joe", player.getName());
-		}
 
-		@Test
-		public void testgetPoint() {
-			//when
-			player.setPoints(100);
-			//then
-			assertEquals(100, player.getPoints());
-		}
-
-		@Test
-		public void testSetPoint() {
-			//when
-			player.setPoints(100);
-			//then
-			assertEquals(100, player.getPoints());
-		}
-		
-		@Test
-		public void testSetPasswordHash() {
-			//when
-			player.setPasswordHash(100);
-			//then
-			assertEquals(100, player.getPasswordHash());
-		}
-		
-		@Test
-		public void testGetPasswordHash() {
-			//when
-			player.setPasswordHash(100);
-			//then
-			assertEquals(100, player.getPasswordHash());
-		}
-		
-		@Test
-		public void testsetID() {
-			//when
-			player.setID("100");
-			//then
-			assertEquals(100, player.getID());
-		}
-		
-		@Test
-		public void testgetID() {
-			//when
-			player.setID("100");
-			//then
-			assertEquals(100, player.getID());
-		}
-
+	@Before
+	public void setUp() throws Exception {
+		// given
+		player = new BasePlayer("100", 10, "", 100);
+		// public BasePlayer(String playerID, int playerPasswordHash, String playerName,int playerPoints) {
 	}
+
+	@Test
+	public void testsetName() {
+		// when
+		player.setName("joe");
+		// then
+		assertEquals("joe", player.getName());
+	}
+
+	public void testgetName() {
+		// when
+		player.setName("joe");
+		// then
+		assertEquals("joe", player.getName());
+	}
+
+	@Test
+	public void testgetPoint() {
+		// when
+		player.setPoints(100);
+		// then
+		assertEquals(100, player.getPoints());
+	}
+
+	@Test
+	public void testSetPoint() {
+		// when
+		player.setPoints(100);
+		// then
+		assertEquals(100, player.getPoints());
+	}
+
+	@Test
+	public void testSetPasswordHash() {
+		// when
+		player.setPasswordHash(100);
+		// then
+		assertEquals(100, player.getPasswordHash());
+	}
+
+	@Test
+	public void testGetPasswordHash() {
+		// when
+		player.setPasswordHash(100);
+		// then
+		assertEquals(100, player.getPasswordHash());
+	}
+
+	@Test
+	public void testsetID() {
+		// when
+		player.setID("100");
+		// then
+		assertEquals(100, player.getID());
+	}
+
+	@Test
+	public void testgetID() {
+		// when
+		player.setID("100");
+		// then
+		assertEquals(100, player.getID());
+	}
+
+}
