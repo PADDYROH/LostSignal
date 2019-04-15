061bdf7a547e248b3bae8fef2ebc5807011c2102
Pat's PC <Pat's PC@DESKTOP-PLGUNPC>
2019-04-15 17:33:14 +1000

    CLimanager

--- Mini_Chess/src/view/CLIManager.java
+++ Mini_Chess/src/view/CLIManager.java
@@ -3,23 +3,41 @@ package view;
 public class CLIManager implements UserInterfaceManager {
 	private int boardwidth;
 	private int boardlength;
-	
+
 	public CLIManager(int boardlength,  int boardwidth) {
 
 	this.boardlength = boardlength;
 	this.boardwidth = boardwidth;
 	
-		
-		String[] chess = new String[boardlength];
+	//piece.rook()
+	//piece.getpiece()	
+	
+		String[][] chess = new String[boardwidth][boardlength];
 		for (int row = 0; row < boardwidth; row++) {
 			System.out.println("");
-			System.out.println("____________");
+			System.out.println("_____________________________________");
 			for (int column = 0; column < boardwidth; column++) {
-				System.out.println("| "+ "CLIManager(piece.getposX, piece.getY)"+ "   ");
+				if( /*piece position */ = chess[row][column]) {
+				System.out.print("| "+ "*"+ "   ");
+				}else{
+					System.out.print("| "+ " "+ "   ");	
+				}
 			}
 				System.out.println("|");
 		}
 		System.out.println(" ");
-		System.out.println("____________");
+		System.out.println("_____________________________________");
+	}
+
+	public void getPiece() {
+		//return this.Piece;
+	}
+
+	public void getGameBoard() {
+
+	}
+
+	public void getchessbardarray() {
+
 	}
 }
