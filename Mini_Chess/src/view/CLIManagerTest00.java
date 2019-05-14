586cdd394f1d7b08fb4f962d0353acadba21703c
PADDYROH <Pat's PC@DESKTOP-PLGUNPC>
2019-05-14 23:47:59 +1000

    Junit for revert gameboard

--- Mini_Chess/src/view/CLIManagerTest00.java
+++ Mini_Chess/src/view/CLIManagerTest00.java
@@ -6,25 +6,63 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import model.GameBoardImpl;
 import model.GameEngine;
 import model.GameEngineImpl;
 
 public class CLIManagerTest00 {
 	CLIManager mainManager;
 	GameEngine mainEngine;
+	GameBoardImpl GameBoard;
+	Object NewBoard;
+	
 	@Before
 	public void setUp() throws Exception {
 		GameEngine mainEngine = new GameEngineImpl();
 		mainManager = new CLIManager(mainEngine);
 	}
-
-	@After
-	public void tearDown() throws Exception {
+	
+	@Test
+	public void saveGameboard() throws Exception {
+		GameBoard.movePiece("r1", 0, 1);
+		Object oldGameBoard;
+		GameBoard.saveGameBoard(oldGameBoard);
+		assertEquals(true,oldGameBoard.getPiece(0, 1));
+	}
+	
+	@Before
+	public void revert() throws Exception {
+		GameEngine mainEngine = new GameEngineImpl();
+		mainManager = new CLIManager(mainEngine);
+		GameBoard.movePiece("r1", 0, 1);
 	}
 
 	@Test
-	public void test() {
-		mainManager.updateBoard(true);
+	public void revertBGameBoard() throws Exception {
+		GameBoard.movePiece("r1", 0, 1);
+		GameBoard.movePiece("r1", 0, 2);
+		GameBoard.revert;
+		assertEquals(true, numTurns = 2);
+		assertEquals(true,GameBoard.getPiece(0, 1));
 	}
+	
+	@Test
+	public void updateRevertGameboard() throws Exception {
+		GameBoard.movePiece("r1", 0, 1);
+		GameBoard.updateGameBoard;
+		assertEquals(true,GameBoard.updateGameBoard.getPiece(0, 1));
+	}
+
+	@Test
+	public void revertWGameBoard() throws Exception {
+		GameBoard.movePiece("r1w", 0, 1);
+		GameBoard.movePiece("r1w", 0, 2);
+		GameBoard.revert;
+		assertEquals(true,GameBoard.getPiece(0, 1));
+	}
+//	@Test
+	//public void test() {
+		//mainManager.updateBoard(true);
+	//}
 
 }
