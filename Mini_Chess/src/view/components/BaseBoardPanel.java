ade9889b8838e7517524f233b211c79dc32c1fb9
s3707187 <s3707187@student.rmit.edu.au>
2019-05-25 21:39:56 +1000

    gui again

--- Mini_Chess/src/view/components/BaseBoardPanel.java
+++ Mini_Chess/src/view/components/BaseBoardPanel.java
@@ -8,21 +8,40 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class BaseBoardPanel extends JPanel {
-	BaseFrame mainFrame;
+	private BaseFrame mainFrame;
+	private Tile[][] tileArray;
 	
 	public BaseBoardPanel(BaseFrame mainFrame) {
 		this.mainFrame = mainFrame;
+		tileArray = new Tile[6][6];
 		//setBackground(new Color(50, 50, 50));
 		//setForeground(new Color(0, 0, 255));
 		setLayout(new GridLayout(6, 6));
 		setBorder(BorderFactory.createLineBorder(new Color(25, 250, 0)));
 		
-		for(int i = 0; i < 36; i++) {
-			add(new JLabel("square"));
+		for(int r = 0; r < 6; r++) {
+			for(int c = 0; c < 6; c++) {
+				Tile tempTile = new Tile();
+				if((r + c) % 2 == 0) {
+					tempTile.setBackground(Color.GRAY);
+				} else {
+					tempTile.setBackground(Color.WHITE);
+				}
+				tileArray[r][c] = tempTile;
+				add(tempTile);
+			}
 		}
 		
+		updateTiles();
 		
-		
+	}
+	
+	public void updateTiles() {
+		tileArray[0][0].add(new JLabel("hello"));
+		tileArray[0][0].revalidate();
+		tileArray[0][0].repaint();
+		revalidate();
+		repaint();
 	}
 
 }
--- Mini_Chess/src/view/components/Tile.java
+++ Mini_Chess/src/view/components/Tile.java
@@ -1,7 +1,16 @@
 package view.components;
 
+import java.awt.Graphics;
+
 import javax.swing.JComponent;
 
 public class Tile extends JComponent {
-
+	
+	public Tile() {
+		setLayout(null);
+	}
+	public void paint(Graphics g) {
+		g.setColor(getBackground());
+		g.fillRect(0,  0, getWidth(), getHeight());
+	}
 }
