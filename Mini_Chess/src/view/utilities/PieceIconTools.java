adb02bfcc5bdad8e086b83d47ca6d6e11ad4b64f
s3707187 <s3707187@student.rmit.edu.au>
2019-05-29 01:06:10 +1000

    finished

--- Mini_Chess/src/view/utilities/PieceIconTools.java
+++ Mini_Chess/src/view/utilities/PieceIconTools.java
@@ -1,5 +1,53 @@
 package view.utilities;
 
-public class PieceIconTools {
+import java.io.File;
 
+import javax.swing.ImageIcon;
+
+import model.piece.Bishop;
+import model.piece.Piece;
+import model.piece.Rook;
+
+public class PieceIconTools {
+	public static ImageIcon pieceToImageIcon(Piece piece, String color) {
+		// TODO Auto-generated method stub
+		return new ImageIcon(pieceToPath(piece, color));
+	}
+		
+	public static String pieceToPath(Piece piece, String color) {
+		String path = "";
+		if(piece != null) {
+			String colorCode;
+			String type;
+			if(color == "white") {
+				colorCode = "W";
+			} else {
+				colorCode = "B"; 
+			}
+			if(piece instanceof Rook) {
+				type = "R";
+			} else if (piece instanceof Bishop) {
+				type = "B";
+			} else {
+				type = "K";
+			}
+			
+			path = "pieceImages" + File.separator + colorCode + type + ".png";
+		}
+		return path;
+	}
+
+	public static ImageIcon pieceToImageIcon(Piece piece) {
+		return new ImageIcon(pieceToPath(piece));
+	
+	}
+	
+	public static String pieceToPath(Piece piece) {
+		String path = "";
+		if(piece != null) {
+			path = pieceToPath(piece, piece.getColor());
+		}
+		return path;
+		
+	}
 }
