6b02eb2879494093bd1a9aeeb6dddc4964ada440
s3707187 <s3707187@student.rmit.edu.au>
2019-05-31 00:56:04 +1000

    fixed some comments

--- Mini_Chess/src/view/model/GUIModel.java
+++ Mini_Chess/src/view/model/GUIModel.java
@@ -12,7 +12,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import model.GameBoardImpl;
 import model.GameEngine;
 import model.Player;
 import model.piece.Piece;
